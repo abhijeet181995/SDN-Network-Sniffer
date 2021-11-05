e878e82509eaf479ed3927146c8e7d37b1b146f7
Abhijeet Srivastava <abhijeet181995@gmail.com>
2021-11-05 19:45:59 +0530

    Add files via upload

---
+++ topology.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.log import setLogLevel
+from mininet.cli import CLI
+from mininet.node import OVSSwitch, Controller, RemoteController
+from time import sleep
+
+
+class SingleSwitchTopo(Topo):
+
+    def build(self):
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
+
+        a1 = self.addHost('a1')
+        b1 = self.addHost('b1')
+        c1 = self.addHost('c1')
+        d1 = self.addHost('d1')
+        sniffer = self.addHost('sniffer')
+
+        self.addLink(s1, a1)
+        self.addLink(s2, b1)
+        self.addLink(s2, d1)
+        self.addLink(s3, c1)
+        self.addLink(s4, sniffer)
+
+        self.addLink(s1, s2)
+        self.addLink(s2, s3)
+        self.addLink(s3, s4)
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    topo = SingleSwitchTopo()
+    c1 = RemoteController('c1', ip='192.168.56.1')
+    net = Mininet(topo=topo, controller=c1)
+    net.start()
+    CLI(net)
+    net.stop()
